bd784b64946ba248fbb2d5c6557d9c83d74bdfa0
baron <baron@spinormedia.com>
2021-06-08 18:06:42 +0900

    kk

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'page/homePage/widget/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
+          ),
+          body: Home()),
+    );
+  }
+}
